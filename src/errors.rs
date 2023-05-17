b7d2a62abf7ad8db7f63eb01e8ac898196b851ce
Yoav Lavi <yoav@yoavlavi.com>
2023-05-17 18:46:20 +0300

    cleanup

--- src/errors.rs
+++ src/errors.rs
@@ -1,14 +1,7 @@
-//! Errors which may arise from this crate.
 use thiserror::Error;
 
-/// An enum of errors this crate may produce. These are compatible with
-/// `failure` errors.
 #[derive(Debug, Error)]
 pub enum AnalyticsError {
-    /// The given message is too large to be sent to RudderStack's API.
-    #[error("message too large")]
-    MessageTooLarge,
-
     #[error("either user_id or anonymous_id are required")]
     InvalidRequest,
 }
